5e6b96adf72a000b93aebcf6cb2e87777451d16a
Lukasz <lukasz.niezbecki@gmail.com>
2023-04-26 23:03:37 +0200

    html: changed logo img; js and scss: changing logo size while scrolled page

--- js/script.js
+++ js/script.js
@@ -1,17 +1,18 @@
-const navMobile = document.querySelector('.nav-mobile');
+const navMobile = document.querySelector('.nav-mobile')
 const navDesktop = document.querySelector('.nav-desktop')
+const navLogo = document.querySelector('.nav__logo')
 const navBtn = document.querySelector('.hamburger');
-const allNavMobileLinks = document.querySelectorAll('.nav__link-mobile');
-const footerYear = document.querySelector('.footer__year');
+const allNavMobileLinks = document.querySelectorAll('.nav__link-mobile')
+const footerYear = document.querySelector('.footer__year')
 
 const handleNav = () => {
-	navBtn.classList.toggle('is-active'); //specyfikacja ikonki hamburgerowej
-	navMobile.classList.toggle('nav-mobile--active');
+	navBtn.classList.toggle('is-active') //specyfikacja ikonki hamburgerowej
+	navMobile.classList.toggle('nav-mobile--active')
 
 	allNavMobileLinks.forEach((link) => {
 		link.addEventListener('click', () => {
-			navMobile.classList.remove('nav-mobile--active');
-			navBtn.classList.remove('is-active'); //żeby ikonka hamb. nie działała poprawnie co drugie skorzystanie z nawigacji
+			navMobile.classList.remove('nav-mobile--active')
+			navBtn.classList.remove('is-active') //żeby ikonka hamb. nie działała poprawnie co drugie skorzystanie z nawigacji
 		});
 	});
 };
@@ -24,10 +25,19 @@ const handleDesktopNav = () => {
 
 window.addEventListener('scroll', handleDesktopNav)
 
+
+const handleNavLogo = () => {
+	navLogo.classList.toggle('nav__logo--reduction', window.scrollY > 65)
+} //dodawanie klasy, zmniejszającej logo w nawigacji
+
+window.addEventListener('scroll', handleNavLogo)
+
+
+
 const handleCurrentYear = () => {
 	//funkcja umieszczająca aktualny rok w stopce
-	const year = new Date().getFullYear(); //funkcja year, new Date - tworzymy obiekt nowej daty, wywołanie metody getFullYear()
-	footerYear.innerText = year;
+	const year = new Date().getFullYear() //funkcja year, new Date - tworzymy obiekt nowej daty, wywołanie metody getFullYear()
+	footerYear.innerText = year
 };
 
-handleCurrentYear();
+handleCurrentYear()
