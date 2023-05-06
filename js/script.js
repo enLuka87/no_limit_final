2f4634b79c44b2391e49e3e42d5cd5541e91b8ca
Lukasz <lukasz.niezbecki@gmail.com>
2023-05-06 15:51:01 +0200

    added header section to stationary point mobile/desktop with hero img from in-house; navigation structure improved

--- js/script.js
+++ js/script.js
@@ -1,43 +1,40 @@
-const navMobile = document.querySelector('.nav-mobile')
-const navDesktop = document.querySelector('.nav-desktop')
-const navLogo = document.querySelector('.nav__logo')
+const navMobile = document.querySelector('.nav-mobile');
+const navDesktop = document.querySelector('.nav-desktop');
+const navLogo = document.querySelector('.nav__box-logo');
 const navBtn = document.querySelector('.hamburger');
-const allNavMobileLinks = document.querySelectorAll('.nav__link-mobile')
-const footerYear = document.querySelector('.footer__year')
+const allNavMobileLinks = document.querySelectorAll('.nav__link-mobile');
+const footerYear = document.querySelector('.footer__year');
 
 const handleNav = () => {
-	navBtn.classList.toggle('is-active') //specyfikacja ikonki hamburgerowej
-	navMobile.classList.toggle('nav-mobile--active')
+	navBtn.classList.toggle('is-active'); //specyfikacja ikonki hamburgerowej
+	navMobile.classList.toggle('nav-mobile--active');
 
 	allNavMobileLinks.forEach((link) => {
 		link.addEventListener('click', () => {
-			navMobile.classList.remove('nav-mobile--active')
-			navBtn.classList.remove('is-active') //żeby ikonka hamb. nie działała poprawnie co drugie skorzystanie z nawigacji
+			navMobile.classList.remove('nav-mobile--active');
+			navBtn.classList.remove('is-active'); //żeby ikonka hamb. nie działała poprawnie co drugie skorzystanie z nawigacji
 		});
 	});
 };
 
-navBtn.addEventListener("click", handleNav)
+navBtn.addEventListener('click', handleNav);
 
 const handleDesktopNav = () => {
-	navDesktop.classList.toggle('nav-desktop--reduction', window.scrollY > 65)
-} //dodawanie klasy, zmniejszającej navigację desktopową
+	navDesktop.classList.toggle('nav-desktop--reduction', window.scrollY > 65);
+}; //dodawanie klasy, zmniejszającej navigację desktopową
 
-window.addEventListener('scroll', handleDesktopNav)
-
+window.addEventListener('scroll', handleDesktopNav);
 
 const handleNavLogo = () => {
-	navLogo.classList.toggle('nav__logo--reduction', window.scrollY > 65)
-} //dodawanie klasy, zmniejszającej logo w nawigacji
+	navLogo.classList.toggle('nav__box-logo--reduction', window.scrollY > 65);
+}; //dodawanie klasy, zmniejszającej logo w nawigacji
 
-window.addEventListener('scroll', handleNavLogo)
-
-
+window.addEventListener('scroll', handleNavLogo);
 
 const handleCurrentYear = () => {
 	//funkcja umieszczająca aktualny rok w stopce
-	const year = new Date().getFullYear() //funkcja year, new Date - tworzymy obiekt nowej daty, wywołanie metody getFullYear()
-	footerYear.innerText = year
+	const year = new Date().getFullYear(); //funkcja year, new Date - tworzymy obiekt nowej daty, wywołanie metody getFullYear()
+	footerYear.innerText = year;
 };
 
-handleCurrentYear()
+handleCurrentYear();
